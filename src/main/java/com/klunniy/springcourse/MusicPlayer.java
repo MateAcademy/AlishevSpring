7e62fc235b0d9359cf1a992ed60259cd2d9f0fe5
Sergey Klunniy <s.klunniy@gmail.com>
2023-06-03 10:28:00 +0300

    add edits

--- src/main/java/com/klunniy/springcourse/MusicPlayer.java
+++ src/main/java/com/klunniy/springcourse/MusicPlayer.java
@@ -5,6 +5,28 @@ package com.klunniy.springcourse;
  */
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public MusicPlayer() {
     }
