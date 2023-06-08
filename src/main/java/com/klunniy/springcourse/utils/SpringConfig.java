c079983550b1de8b78c54ab2b159594d60c5d0fe
Serhii Klunniy <s.klunniy@gmail.com>
2023-06-08 13:57:33 +0300

    commit

--- src/main/java/com/klunniy/springcourse/utils/SpringConfig.java
+++ src/main/java/com/klunniy/springcourse/utils/SpringConfig.java
@@ -7,17 +7,22 @@ import org.springframework.context.annotation.*;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Этот класс содержит в себе конфигурацию spring приложения
+ */
 @Configuration
 @ComponentScan("com.klunniy.springcourse")
 @PropertySource("classpath:musicPlayer.properties")
 public class SpringConfig {
 
     @Bean
+    @Scope("singleton")
     public List<Song> classicalList() {
         return Arrays.asList(new Classic1(), new Classic2(), new Classic3());
     }
 
     @Bean
+    @Scope("singleton")
     public List<Song> rocklList() {
         return Arrays.asList(new Rock1(), new Rock2(), new Rock3());
     }
